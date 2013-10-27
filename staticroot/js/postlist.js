53b9bc8d81a67c7ddf6ae208aff5e2c80d5552fb
David Ketcheson <dketch@gmail.com>
2013-10-27 21:19:38 +0300

    Make post lists look even more like Gmail.
    Grey with white borders.
    Clickable anywhere.
    Title + text + date.
    Truncate text based on browser window size.

--- staticroot/js/postlist.js
+++ staticroot/js/postlist.js
@@ -3,6 +3,8 @@ $(window).load(function(){
 $('#post-list dt').click(function (e) {
     //Toggle open/close on the <div> after the <dt>, opening it if not open.
     $(e.target).next('dd').slideToggle('fast');
+    //The next line makes it work also when clicking on an element inside the dt:
+    $(e.target).parent().next('dd').slideToggle('fast');
 });
 });
 
